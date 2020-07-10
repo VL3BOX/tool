8427ea2f5d84bebffc802079892f0c7d2af39f9b
iRuxu <rx6@qq.com>
2020-07-10 15:46:30 +0800

    update

--- src/components/list.vue
+++ src/components/list.vue
@@ -373,7 +373,7 @@ export default {
             if(val){
                 return showMinibanner(val)
             }else{
-                return __imgPath + 'image/banner/tool' + subtype + '.png?v=1'
+                return __ossMirror + 'image/banner/tool' + subtype + '.png?v=1'
             }
             return this.defaultBanner;
         },
