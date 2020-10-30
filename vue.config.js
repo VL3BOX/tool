caaa9af1600c04cfaec0868289580d3871853084
iRuxu <rx6@qq.com>
2020-10-30 22:44:27 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -22,16 +22,17 @@ module.exports = {
     // },
 
     //❤️ Porxy ~
-    // devServer: {
-    //     proxy: {
-    //         "/api": {
-    //             "target": process.env["DEV_SERVER"] == "true" ? "http://localhost:51818" : "https://api.jx3box.com",
-    //             "onProxyReq": function (request) {
-    //                 request.setHeader("origin", "");
-    //             }
-    //         }
-    //     }
-    // },
+    devServer: {
+        proxy: {
+            "/api": {
+                "target": "https://next.jx3box.com",
+                "onProxyReq": function (request) {
+                    request.setHeader("origin", "");
+                }
+            }
+        },
+        disableHostCheck: true
+    },
 
     //webpack配置
     // configureWebpack: (config) => {
