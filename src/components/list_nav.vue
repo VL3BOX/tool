286093acdef08b525e7c81eba4d5260e829323c9
iRuxu <rx6@qq.com>
2021-02-05 17:18:38 +0800

    update sidebar

--- src/components/list_nav.vue
+++ src/components/list_nav.vue
@@ -12,13 +12,82 @@
             <!-- <a href=""><i class="el-icon-menu"></i><span>站内应用</span></a> -->
         </div>
 
-        <!-- <h5 class="u-title">应用推荐</h5>
-        <div class="m-nav-group">
-        </div> -->
+        <h5 class="u-title">魔盒应用</h5>
+        <div class="m-app-box">
+            <a class="u-app" href="/app/database" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('database')"/></i
+                ><span>数据库</span></a
+            >
+            <a class="u-app" href="/app/icons" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('icons')"/></i
+                ><span>图标库</span></a
+            >
+            <a class="u-app" href="/app/talent"  target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('talent')"/></i
+                ><span>奇穴模拟</span></a
+            >
+            <a class="u-app" href="/app/macroeditor"  target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('macroeditor')"/></i
+                ><span>宏编辑器</span></a
+            >
+            <a class="u-app" href="/app/translator"  target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('translator')"/></i
+                ><span>繁體轉換</span></a
+            >
+            <a class="u-app" href="/team" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('team')"/></i
+                ><span>团队管理</span></a
+            >
+            <a class="u-app" href="/dbm" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('dbm')"/></i
+                ><span>数据构建</span></a
+            >
+            <a class="u-app" href="/fb/#/skill" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('fbdata')"/></i
+                ><span>副本技能</span></a
+            >
+            <a class="u-app" href="/fb/#/drop" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('fbdrop')"/></i
+                ><span>副本掉落</span></a
+            >
+            <a class="u-app" href="/fb/#/gem" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('fbgem')"/></i
+                ><span>瑰石查询</span></a
+            >
+            <a class="u-app" href="/app/servers" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('servers')"/></i
+                ><span>开服监控</span></a
+            >
+            <a class="u-app" href="/app/price" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('price')"/></i
+                ><span>金价走势</span></a
+            >
+            <a class="u-app" href="/app/flower" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('flower')"/></i
+                ><span>花价查询</span></a
+            >
+            <a class="u-app" href="/app/furniture" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('furniture')"/></i
+                ><span>家具图鉴</span></a
+            >
+            <a class="u-app" href="/app/facedata" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('facedata')"/></i
+                ><span>妆容解析</span></a
+            >
+            <a class="u-app" href="https://j3cx.com/serendipity" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('qiyu')"/></i
+                ><span>奇遇查询</span
+                ></a>
+            <a class="u-app" href="https://j3cx.com/exam/" target="_blank"
+                ><i class="u-app-icon"><img :src="getIcon('keju')"/></i
+                ><span>科举题库</span
+                ></a>
+        </div>
     </div>
 </template>
 
 <script>
+import {__imgPath} from '@jx3box/jx3box-common/js/jx3box.json'
 export default {
     name: "list_nav",
     props: [],
@@ -66,6 +135,9 @@ export default {
     methods: {
         typeLink : function (subtype){
             return './?subtype=' + subtype
+        },
+        getIcon : function (key){
+            return __imgPath + 'image/box/' + key + '.svg'
         }
     },
     mounted: function() {
