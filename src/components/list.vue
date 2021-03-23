03f02f61ac75905d6667eaa1c93d09e710603b92
litthy <litthy@qq.com>
2021-03-23 16:07:48 +0800

    atd

--- src/components/list.vue
+++ src/components/list.vue
@@ -37,7 +37,7 @@
             </div>
             <template slot="filter">
                 <!-- 版本过滤 -->
-                <clientBy @filter="filter" type="std"></clientBy>
+                <clientBy @filter="filter" :type=""></clientBy>
                 <!-- 角标过滤 -->
                 <markBy @filter="filter"></markBy>
                 <!-- 排序过滤 -->
@@ -276,7 +276,13 @@ export default {
     },
     created: function() {
         this.page = ~~this.$route.query.page || 1
-        this.loadPosts()
+        let query = new URLSearchParams(location.search);
+        let client = (this.$route && this.$route.query.client) || query.get("client");
+        if(client){
+            this.client = client
+        }else{
+            this.client = 'std'
+        }
     },
     components: {
         listbox
