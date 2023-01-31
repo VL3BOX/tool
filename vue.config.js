5f515549e09c69aa8e110f3933570c2e8bbae63e
iRuxu <rx6@qq.com>
2023-02-01 05:10:25 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -57,6 +57,12 @@ module.exports = {
                     request.setHeader("origin", "");
                 },
             },
+            "/api/cny": {
+                target: "https://pay.jx3box.com/",
+                onProxyReq: function(request) {
+                    request.setHeader("origin", "");
+                },
+            },
             "/api": {
                 "target": "https://next2.jx3box.com",
                 "onProxyReq": function (request) {
@@ -112,22 +118,22 @@ module.exports = {
         (process.env.NODE_ENV === 'development' && '/') ||
 
         //BY origin
-        (process.env.STATIC_PATH === "origin" && `${JX3BOX.__staticPath["origin"]}${pkg.name}/`) || 
+        (process.env.STATIC_PATH === "origin" && `${JX3BOX.__staticPath["origin"]}${pkg.name}/`) ||
 
         //BY github
-        (process.env.STATIC_PATH === "github" && `${JX3BOX.__staticPath["github"]}${pkg.name}/`) || 
+        (process.env.STATIC_PATH === "github" && `${JX3BOX.__staticPath["github"]}${pkg.name}/`) ||
 
         //BY jsdelivr
-        (process.env.STATIC_PATH === "jsdelivr" && `${JX3BOX.__staticPath["jsdelivr"]}${pkg.name}@gh-pages/`) || 
+        (process.env.STATIC_PATH === "jsdelivr" && `${JX3BOX.__staticPath["jsdelivr"]}${pkg.name}@gh-pages/`) ||
 
         //BY OSS=>CDN
         (process.env.STATIC_PATH === "mirror" && `${JX3BOX.__staticPath["mirror"]}${pkg.name}/`) ||
 
         //BY relative path
-        (process.env.STATIC_PATH === "repo" && `/${pkg.name}/`) || 
+        (process.env.STATIC_PATH === "repo" && `/${pkg.name}/`) ||
 
         //BY root path or bind a domain
-        (process.env.STATIC_PATH == 'root' && '/') || 
+        (process.env.STATIC_PATH == 'root' && '/') ||
 
         //for lost
         '/',
