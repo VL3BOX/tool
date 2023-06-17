9f5b42ee6f27cc78976cf95bc21029b22a83546e
Kaviilee <jiaweilee95@126.com>
2023-06-17 21:45:10 +0800

    chore: update collection api & pkg

--- src/service/helper.js
+++ src/service/helper.js
@@ -1,20 +1,10 @@
-import { $helper, $cms } from "@jx3box/jx3box-common/js/https";
+import { $cms } from "@jx3box/jx3box-common/js/https";
 import axios from "axios";
-
-// 关联文档
-function getCollection($collection_id, params = {}) {
-    return $helper()({
-        method: "GET",
-        url: `/api/post/collection/${$collection_id}`,
-        params: params,
-    });
-}
 
 const dict_url = `https://cdn.jsdelivr.net/gh/JX3BOX/jx3box-dict@master/dict.json?v=${Date.now()}`;
 function getNewDict() {
     return axios.get(dict_url);
 }
-
 
 // 公告
 function getBread(key) {
@@ -34,4 +24,4 @@ function getMenuGroups(keys) {
     });
 }
 
-export { getCollection, getNewDict, getMenuGroup, getBread, getMenuGroups };
+export { getNewDict, getMenuGroup, getBread, getMenuGroups };
